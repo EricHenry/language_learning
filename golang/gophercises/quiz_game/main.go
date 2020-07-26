28db4d29916231e5463ac1b993c738de2453512d
Eric Correia <correia.eh@gmail.com>
2020-07-26 09:29:45 -0400

    cleanly inturrupt game on timer completion or game completion

--- golang/gophercises/quiz_game/main.go
+++ golang/gophercises/quiz_game/main.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
+	"time"
 )
 
 // Answer is an answer to a question
@@ -15,18 +17,50 @@ type Answer struct {
 	actual   string
 }
 
+func cleanString(s string) string {
+	return strings.ToLower(strings.Trim(s, " "))
+}
+
 func correctAnsers(as []Answer) int {
 	result := 0
 	for _, answer := range as {
-		if answer.expected == answer.actual {
+		if cleanString(answer.expected) == cleanString(answer.actual) {
 			result = result + 1
 		}
 	}
 	return result
 }
 
+func startTimer(secs time.Duration, c chan string) {
+	time.Sleep(secs)
+	fmt.Println("CLOSING, times up")
+	c <- "Times Up"
+	close(c)
+}
+
+func runGame(as *[]Answer, r *csv.Reader, c chan string) {
+	for {
+		line, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("%v? \n", line[0])
+		var guess string
+		fmt.Scanln(&guess)
+
+		*as = append(*as, Answer{
+			expected: line[1],
+			actual:   guess,
+		})
+	}
+	fmt.Println("CLOSING, game loop complete")
+	c <- "Completed"
+	close(c)
+}
+
 func main() {
 	loc := flag.String("loc", "problems.csv", "The location of the csv file, where the first column is the question and the second column is the answer")
+	gameTime := flag.Uint("game-time", 30, "The game time in seconds.")
 	flag.Parse()
 
 	csvFile, err := os.Open(*loc)
@@ -36,21 +70,34 @@ func main() {
 		fmt.Println("Couldn't open problem file")
 	}
 
-	var answers []Answer
+	fmt.Println("Welcome to the Quiz Game!")
+	fmt.Printf("You will have %v seconds to complete as many questions as you can\n", *gameTime)
 	for {
-		line, err := reader.Read()
-		if err == io.EOF {
+		fmt.Println("Are you ready to start the game? (y/n)")
+		var ready string
+		fmt.Scanln(&ready)
+
+		if ready == "y" {
 			break
 		}
-		fmt.Printf("%v? \n", line[0])
-		var guess string
-		fmt.Scanln(&guess)
-
-		answers = append(answers, Answer{
-			expected: line[1],
-			actual:   guess,
-		})
 	}
 
-	fmt.Printf("You got %v out of %v correct!", len(answers), len(answers))
+	var answers []Answer
+	gameDuration := time.Duration(*gameTime) * time.Second
+	gameStatus := make(chan string, 1)
+	go startTimer(gameDuration, gameStatus)
+	go runGame(&answers, reader, gameStatus)
+
+	for status := range gameStatus {
+		switch status {
+		case "Times Up":
+			fmt.Println("\nTime ran out!")
+			break
+		case "Complete":
+			fmt.Println("\nAnswered all questions!")
+			break
+		}
+	}
+
+	fmt.Printf("You got %v out of %v correct!\n", correctAnsers(answers), len(answers))
 }
